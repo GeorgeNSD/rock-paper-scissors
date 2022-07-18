a338605beb8078ed96f154518912314c31dc0c6f
george <soursos@windowslive.com>
2022-07-18 14:39:08 +0300

    Get player choice

--- javascript.js
+++ javascript.js
@@ -1,7 +1,25 @@
 function getComputerChoice(){
     let x = Math.random()*3;
-    return Math.floor(x);
+    x = Math.floor(x);
+    if (x===0){
+        return "rock";
+    } else if (x===1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 let computerChoice = getComputerChoice();
-console.log(computerChoice)
+console.log(computerChoice);
+
+function playerSelection(){
+    let choice;
+    do {
+        choice = prompt("Enter your choice:");
+        choice = choice.toLowerCase();
+    } while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
+    console.log(choice);
+}
+
+let playerChoice = playerSelection();
