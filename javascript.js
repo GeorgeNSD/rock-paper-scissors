d77e357e914ea9fb2b78cb65b52d2db79c257efe
george <soursos@windowslive.com>
2022-07-18 18:27:46 +0300

    Round complete

--- javascript.js
+++ javascript.js
@@ -2,11 +2,11 @@ function getComputerChoice(){
     let x = Math.random()*3;
     x = Math.floor(x);
     if (x===0){
-        return "rock";
+        return "Rock";
     } else if (x===1){
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
@@ -17,9 +17,22 @@ function playerSelection(){
     let choice;
     do {
         choice = prompt("Enter your choice:");
-        choice = choice.toLowerCase();
-    } while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
-    console.log(choice);
+        choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+    } while (choice !== "Rock" && choice !== "Paper" && choice !== "Scissors");
+    return choice;
 }
 
 let playerChoice = playerSelection();
+console.log(playerChoice);
+
+function whoWins(cChoice, pChoice){
+    if (cChoice === pChoice){
+        console.log(`Draw! You both picked ${cChoice}!`);
+    } else if (cChoice === "Rock" && pChoice === "Paper" || cChoice === "Paper" && pChoice === "Scissors" || cChoice === "Scissors" && pChoice === "Rock"){
+        console.log(`You Win! ${pChoice} beats ${cChoice}`)
+    } else {
+        console.log(`You lose! ${cChoice} beats ${pChoice}`)
+    }
+}
+
+whoWins(computerChoice, playerChoice);
